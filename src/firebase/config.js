2301ecf95de0e7e2a043cdc2a789dbe4fde27884
hirosan-blog <hiro2513omoro@ezweb.ne.jp>
2021-05-17 14:20:01 +0900

    firebase接続

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCeRAW5652_OK5ALYPuno407EKkd9JhIz0",
+  authDomain: "react-ecsite.firebaseapp.com",
+  projectId: "react-ecsite",
+  storageBucket: "react-ecsite.appspot.com",
+  messagingSenderId: "229411312538",
+  appId: "1:229411312538:web:af418fec26736924f92023",
+  measurementId: "G-TF2PN32015"
+};
